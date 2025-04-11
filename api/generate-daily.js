856262eec187ef8419fd61b8d8e58cc809f521ec
flammabl3 <harryjung135@gmail.com>
2025-04-11 15:05:49 -0600

    old word removal

--- api/generate-daily.js
+++ api/generate-daily.js
@@ -1,5 +1,7 @@
 import wordsArray from "./words.json";
 import {addDay, dateDocExists} from "../_services/scores-service";
+import path from "path";
+import fs from "fs";
 
 export default async function handler(req, res) {
     const today = new Date().toLocaleDateString("en-GB").split("/").join(".");
@@ -10,6 +12,12 @@ export default async function handler(req, res) {
 
     if (exists == false) {
         const word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+
+        const updatedWordsArray = wordsArray.filter((w) => w !== word);
+
+        const filePath = path.resolve("./api/words.json");
+        fs.writeFileSync(filePath, JSON.stringify(updatedWordsArray, null, 2));
+
         await addDay(today, word);
 
         console.log("Generated word:", word);
